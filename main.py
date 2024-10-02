b87b878b36db2506ff8406969e6998acaa52c1cf
a.broquard <a.broquard@kayrros.com>
2024-10-02 10:47:43 +0200

    req

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import streamlit as st
 import pandas as pd
-from chu_cheat_sheet.prescribe import get_prescription
 
 facteurs = pd.read_csv("data/facteurs.csv")
 facteurs = facteurs[[elt for elt in facteurs.columns if not elt.startswith("Unn")]]
+
+def get_prescription(type_cesar, facteurs):
+    if len(facteurs[facteurs["is_majeur"]])!=0:
+        return "Anti coagulation 6 semaines + BAT"
+
+    if len(facteurs[~facteurs["is_majeur"]])>=2:
+        if type_cesar=="programmee":
+            return "BAT + anticoagulation 7 jours"
+        if type_cesar=="urgence":
+            return "Anticoagulation 6 semaines"
+
+    if len(facteurs[~facteurs["is_majeur"]])<=1 and type_cesar=="programmee":
+            return "Pas anticoagulation + BAT 7 jours"
+
+    if len(facteurs[~facteurs["is_majeur"]])==0 and type_cesar=="urgence":
+        return "Pas anticoagulation + BAT 7 jours"
+
+    if len(facteurs[~facteurs["is_majeur"]])==1 and type_cesar=="urgence":
+        return "Anticoagulation 7 jours  + BAT"
+
+    return "Pas de diagnostic"
 
 
 
